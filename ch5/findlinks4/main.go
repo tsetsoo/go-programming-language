4458fab840dd8b444d495cacecbb0c6abc3af8a6
Tsvetelin Pantev <tsvetelin.pantev@gmail.com>
2022-05-23 23:47:56 +0300

    ch5 ex13

--- ch5/findlinks4/main.go
+++ ch5/findlinks4/main.go
@@ -2,9 +2,12 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
@@ -36,22 +39,54 @@ func crawl(url string) []string {
 	return list
 }
 
-func extract(url string) ([]string, error) {
-	resp, err := http.Get(url)
-
+func extract(urlToCheck string) ([]string, error) {
+	originalUrl, err := url.Parse(urlToCheck)
 	if err != nil {
 		return nil, err
 	}
-	originalHost := resp.Request.URL.Host
+	resp, err := http.Get(urlToCheck)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		resp.Body.Close()
-		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+		return nil, fmt.Errorf("getting %s: %s", urlToCheck, resp.Status)
+	}
+	forwardedHost := resp.Request.URL.Host
+	if forwardedHost != originalUrl.Host {
+		return []string{}, nil
+	}
+	var fileName string
+	currentPath, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+	dirPath := currentPath + "/crawled"
+	fullPath := resp.Request.URL.Path
+	if len(fullPath) > 0 {
+		fullPath = removeTrailingSlash(fullPath)
+		fullPathSplit := strings.Split(fullPath, "/")
+		if len(fullPathSplit) > 1 {
+			fileName = fullPathSplit[len(fullPathSplit)-1]
+			dirPath += strings.Join(fullPathSplit[:len(fullPathSplit)-1], "/")
+			fmt.Println(dirPath)
+			createDir(dirPath)
+		} else {
+			fileName = fullPath
+		}
+		if !strings.HasSuffix(fileName, ".html") {
+			fileName = fileName + ".html"
+		}
+		dirPath += "/"
+	} else {
+		createDir(dirPath)
+		fileName = "/home.html"
 	}
 
 	doc, err := html.Parse(resp.Body)
-	resp.Body.Close()
+
 	if err != nil {
-		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+		return nil, fmt.Errorf("parsing %s as HTML: %v", urlToCheck, err)
 	}
 
 	var links []string
@@ -65,7 +100,7 @@ func extract(url string) ([]string, error) {
 				if err != nil {
 					continue // ignore bad URLs
 				}
-				if link.Host != originalHost {
+				if link.Host != originalUrl.Host {
 					continue // skip links outside of domain
 				}
 				linkString := link.String()
@@ -73,14 +108,7 @@ func extract(url string) ([]string, error) {
 				if len(linkParts) > 1 {
 					linkString = strings.Join(linkParts[:len(linkParts)-1], "#") // remove anchor tag
 				}
-				j := len(linkString)
-				_, size := utf8.DecodeLastRuneInString(linkString[:j])
-				j -= size
-
-				lastByRune := linkString[j:]
-				if lastByRune == "/" {
-					linkString = linkString[:j]
-				}
+				linkString = removeTrailingSlash(linkString)
 				fileExtension := filepath.Ext(linkString)
 				if len(fileExtension) > 0 && fileExtension != ".html" {
 					continue //skip downloadable links
@@ -89,8 +117,25 @@ func extract(url string) ([]string, error) {
 			}
 		}
 	}
+	f, err := os.Create(dirPath + fileName)
+	if err != nil {
+		return nil, fmt.Errorf("creating file at %s: %v", dirPath+fileName, err)
+	}
+	defer f.Close()
+	fmt.Println("path is ", dirPath+fileName)
+	var htmlPageBuffer bytes.Buffer
+	err = html.Render(&htmlPageBuffer, doc)
+	if err != nil {
+		return nil, fmt.Errorf("parsing html: %v", err)
+	}
+	stringHtm := htmlPageBuffer.String()
+	// fmt.Println(stringHtm)
+	_, err = f.WriteString(stringHtm)
+	if err != nil {
+		return nil, fmt.Errorf("writing to file: %v", err)
+	}
+	f.Sync()
 	forEachNode(doc, visitNode, nil)
-	fmt.Println(doc.Data)
 	return links, nil
 }
 
@@ -103,6 +148,27 @@ func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
 	}
 	if post != nil {
 		post(n)
+	}
+}
+
+func removeTrailingSlash(withPossibleTrailingSlash string) string {
+	j := len(withPossibleTrailingSlash)
+	_, size := utf8.DecodeLastRuneInString(withPossibleTrailingSlash[:j])
+	j -= size
+
+	lastByRune := withPossibleTrailingSlash[j:]
+	if lastByRune == "/" {
+		withPossibleTrailingSlash = withPossibleTrailingSlash[:j]
+	}
+	return withPossibleTrailingSlash
+}
+
+func createDir(path string) {
+	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+		err := os.MkdirAll(path, os.ModePerm)
+		if err != nil {
+			log.Println(err)
+		}
 	}
 }
 
